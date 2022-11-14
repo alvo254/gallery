61deb61880c6d6b10e7910ab276f64433cb899ab
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-14 17:42:12 +0300

    Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+## Creating a collection
+db.createCollection("<collection name>")
--- _config.js
+++ _config.js
@@ -4,7 +4,7 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     // development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    development: 'mongodb+srv://alvo:<password>@cluster0.lmxs8me.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://alvo:barry1234@gallery.uowqa7l.mongodb.net/?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
