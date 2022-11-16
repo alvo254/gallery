408d427dbd89b6e7c083c64a5545587aaefe2bb9
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-16 14:50:34 +0300

    test addition

--- _config.js
+++ _config.js
@@ -3,7 +3,6 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    // development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     development: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
     test: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
 }
