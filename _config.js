785ea8c14863332142fb9ad70c283e66b1023feb
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-16 15:15:22 +0300

    production config

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    production: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
     development: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
     test: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
 }
