91881f7d1eb6652d7a077d081593e5fc794946a2
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-16 14:27:37 +0300

    push to development

--- _config.js
+++ _config.js
@@ -5,6 +5,6 @@ config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     // development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     development: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://alvo:barry1234@gallery.svei8sh.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
